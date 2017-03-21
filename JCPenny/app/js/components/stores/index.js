116fc34c439dddffc3dd893143b0f9d1cc82d837
Soni, Mayur Kumar (US - Mumbai) <maysoni@deloitte.com>
2017-03-21 18:47:05 +0530

    Store UI changes

--- JCPenny/app/js/components/stores/index.js
+++ JCPenny/app/js/components/stores/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View, ScrollView } from 'react-native';
+import { TouchableOpacity, View, ScrollView, ListView, TouchableHighlight} from 'react-native';
 import { connect } from 'react-redux';
 import { actions } from 'react-native-navigation-redux-helpers';
 import { Container, Title, Content, Text, Button, Icon, Left, Body, Right, Spinner, Thumbnail, List, ListItem} from 'native-base';
@@ -31,15 +31,17 @@ class Stores extends Component {
 
   constructor(props) {
     super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
     this.state = {
       results: [],
       title: 'Stores',
       headerIconDetails: {
-        leftHeaderIcon:'ios-arrow-back',
-        leftHeaderIconAction: 'popRoute',
         rightHeaderIcon: 'search',
         rightHeaderIconAction: 'search'  
       },
+      
+      dataSource: ds.cloneWithRows([{name:'Loading', image: {url:'https://m.jcpenney.com/v4/stores/zipcode=80202'}}])
     }
   }
 
@@ -61,15 +63,17 @@ class Stores extends Component {
     });
 
     
-    return fetch('https://m.jcpenney.com/v4/categories/root')
+    return fetch('https://m.jcpenney.com/v4/stores/zipcode=80202')
       .then((response) => response.json())
       .then((responseJson) => {
           // Store the results in the state variable results and set loading to 
           // false to remove the spinner and display the list of categories
-          that.setState({
-              results: responseJson.groups[0].categories,
-              loading: false
-          });
+          const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+           that.setState({
+            results: responseJson.stores,
+            loading: false,
+            dataSource:ds.cloneWithRows(responseJson.stores)
+        });
       })
       .catch((error) => {
 
@@ -82,20 +86,61 @@ class Stores extends Component {
   }
 
   render() {
+    console.log(this.state.results);
+
+    let counter = 0;
     return (
       <Container style={styles.container}>
-        <HeaderBar title={this.state.title} headerIconDetails={this.state.headerIconDetails} />
-      
-        <Content>
-          <Grid style={styles.mt}>
-           <Text>Stores</Text>
-    
-          </Grid>
-        </Content>
+      <HeaderBar title="Find Store" headerIconDetails={this.state.headerIconDetails} />  
 
-        <FooterBar />
+        <TouchableHighlight style={{flexDirection:'row', flex:0.1, onPress={this.onMapClick}}}>
+
+          <View style={{flexDirection:'row', padding:15,flex:0.1, borderBottomWidth:1,borderBottomColor:'lightgrey'}}>                    
+          
+            <View>
+              <Icon active name='ios-navigate-outline' style={{color:'red', width:50, height:30,alignSelf:'center'}}/>            
+            </View>   
+
+           <View style={{flexDirection:'column', flex:0.8}}>
+              <Text style={{fontSize:13, fontWeight:'600'}}>823030</Text>
+              <Text style={{height:15, fontSize:12}}>Tap to change location </Text>
+           </View>
+
+           <View style={{flexDirection:'row', alignItems: 'center'}}>
+              <Icon active name='map'/>
+              <Text style={{color:'red', paddingLeft:10,fontSize:12}}>Map</Text>            
+            </View>  
+
+          </View>
+        </TouchableHighlight>
+      
+
+      <Content>                      
+      <ListItem itemHeader first>
+        <Text style={{fontSize:12, color:'grey'}}>Nearby Stores</Text>
+      </ListItem>        
+      {this.state.loading ? 
+        <Spinner /> :                                                     
+        <List dataArray={this.state.results} renderRow={(item) =>                                    
+          <ListItem button onPress={() => this.pushRoute('pdp', item.url)}>                  
+          <View style={{flexDirection:'column', flex:1}}>
+
+          <View style={{flexDirection:'row', justifyContent:'flex-start'}}>
+          <Text style={{fontWeight: 'bold', color: '#696969', fontSize: 14}}>{item.name}</Text>
+          <Text style={{ color: '#696969', fontSize: 14,paddingLeft: 10}}>{item.distance} mi</Text>                      
+          </View>                    
+          <View style={{flexDirection:'row', paddingTop:5}} >
+          <Text style={{color: '#696969', fontSize: 14,flexDirection:'row'}}>{item.street}</Text>                      
+          </View>
+          </View>                      
+          </ListItem>
+        } />
+      }
+      </Content>
+
+      <FooterBar />
       </Container>
-    );
+      );
   }
 }
 
